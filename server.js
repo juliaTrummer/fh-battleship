316768d3bc613ad4c15c4d5b8f83fc72ef98e7a3
truju <julia.trummer@edu.fh-joanneum.at>
2020-05-16 12:48:23 +0200

    Game init

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const db = require('./app/config/db.config.js');
 
 const User = db.users;
 
+app.get("/", function (req, res) {
+    res.render("index");
+})
+
+app.listen(port, function () {
+    console.log("app running")
+})
+
+
 // force: true will drop the table if it already exists
 db.sequelize.sync({force: true}).then(() => {
     console.log('Drop and Resync with { force: true }');
